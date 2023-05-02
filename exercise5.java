6367ddeb48ba9f634b22832dba788519c430b16d
Yoiner Bello Fajardo <ybello@imasa.com.ar>
2023-05-02 14:47:47 -0300

    add exercises5

---
+++ exercise5.java
@@ -0,0 +1,24 @@
+/*
+*           Consider the following code
+*/
+
+import java.util.*;
+public class exercise5 {
+    public static void main(String[] args) {
+        
+        List myList = new ArrayList<>();
+        myList.add("Java");
+        myList.add("is");
+        myList.add("awesome");
+
+        System.out.println(myList);
+    }
+}
+        /*
+        *   a- [Java, is, awesome] <---- Correct answer
+        *   b- [Java, is, cool]
+        *   c- [Java, is, powerful]
+        *   d- [Java, is, great]
+        *   e- Compilation error
+        *   f- Runtime error
+         */
