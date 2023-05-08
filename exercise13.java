d23128a7573a70be783cfeecf5aa33e66fde3cdf
Yoiner Bello Fajardo <ybello@imasa.com.ar>
2023-05-08 10:38:09 -0300

    add exercise13

--- exercise13.java
+++ exercise13.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class exercise13 {
+public class exercise13 {
+    static int total = 5;
+    public static void main(String[] args) {
+        int chocolates = 5;
+        while (chocolates > 0 && total > 0) {
+            chocolates--;
+            total--;
+        }
+        System.out.println(chocolates + " chocolates left");
+    }
 }
