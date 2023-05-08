6a203272ec0f71c560f2af06809ab23ac6a5e8fc
Yoiner Bello Fajardo <ybello@imasa.com.ar>
2023-05-08 09:50:41 -0300

    add exercise12

--- exercise12.java
+++ exercise12.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class exercise23 {
+public class exercise12 {
+    public static void main(String[] args) {
+
+        /*      Tipos de datos que acepta Switch
+        *  int, byte, short, char, String, enum values
+        */
+
+        int dayOfWeek = 0;
+        switch (dayOfWeek) {
+            default:
+                System.out.println("weekday");
+                break;
+            case 0:
+                System.out.println("Sunday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+        }
+    }
 }
