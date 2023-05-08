7871bb8b84adbd1568e9be2ecf540a340e382ee8
Yoiner Bello Fajardo <ybello@imasa.com.ar>
2023-05-08 12:22:43 -0300

    add exercise 17, 18, 19, 20, 21

---
+++ exercise18.java
@@ -0,0 +1,20 @@
+/*
+*       Consider the following Java code. What will be the output of this code?
+*/
+public class exercise18 {
+  int x = 1;
+public static void main(String[] args) {
+    int x = 2;
+    System.out.println(x);
+}
+public void printX() {
+System.out.println(x);}
+}
+
+/*      Output?
+*   a- 1
+*   b- 2 <--- Correct answer
+*   c- Compilation error
+*   d- Runtime error
+*   e- None of the above
+*/
---
+++ exercise19.java
@@ -0,0 +1,21 @@
+/*
+*       Consider the following Java Code. What will be the output of this code?
+*/
+public class exercise19 {
+  public static void main(String[] args) {
+      int a = 6, b = 3, c = 1;
+      int result = a * b / c;   // como no hay parentesis, hay que respetar el orden que es de izquierda a derecha
+      System.out.println(result);
+  }
+}
+
+/*
+*       Output?
+*   a- 2
+*   b- 9
+*   c- 18 <--- Correct answer
+*   d- Compilation error
+*   e- Runtime error
+*   f- None of the above
+*/
+
