cccfee943138abc48595e9948f3befb7f18739a6
Yoiner Bello Fajardo <ybello@imasa.com.ar>
2023-05-08 11:13:07 -0300

    add exercise15

---
+++ exercise15.java
@@ -0,0 +1,16 @@
+public class exercise15 {
+    public static void main(String[] args) {
+    }
+    public void bubble(int[] A) {
+        int i, j, aux;
+        for(i = 0; i < A.length; i++) {
+            for(j = 0; j < A.length -i -1; j++) {
+                if(A[j + 1] < A[j]) {
+                    aux = A[j + 1];
+                    A[j + 1] = A[j];
+                    A[j] = aux;
+                }
+            }
+        }
+    }
+}
