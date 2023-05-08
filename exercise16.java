462ad401f9e271ec6856eaa9c0fa4c311a32e40a
Yoiner Bello Fajardo <ybello@imasa.com.ar>
2023-05-08 11:37:17 -0300

    add exercise16

---
+++ exercise16.java
@@ -0,0 +1,11 @@
+public class exercise16 {
+    public static void main(String[] args) {
+        final String[] names = new String[3];
+        names[0] = "Lucas";
+        names[1] = "Martin";
+        names[2] = "Pepe";
+        for(String name : names) {
+            System.out.println(name);
+        }
+    }
+}
