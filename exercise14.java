942c0f1967f1d2f46afc9c70217b95ddf5035fba
Yoiner Bello Fajardo <ybello@imasa.com.ar>
2023-05-08 10:38:23 -0300

    add exercise14

--- exercise14.java
+++ exercise14.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class exercise14 {
+public class exercise14 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
 }
