8ae2595a0c00f0bcec88c9bf79972ce336001cfd
Yoiner Bello Fajardo <ybello@imasa.com.ar>
2023-05-08 14:53:58 -0300

    add exercise 24,25,26

--- exercise24.java
+++ exercise24.java
@@ -1,10 +1,22 @@
+/*
+*       What is the result of the following snippet?
+*/
 public class exercise24 {
     public static void main(String[] args) {
         int a = 5;
         int b = 2;
         int c = 8;
         int d = 4;
-        int result = c++ - ++a + b-- * d;
+        int result = c++ - ++a + b-- * d; //multiplicamos primero, orden aritmetico.
         System.out.println(result);
     }
 }
+/*
+*       Output?
+*   a- 10 <---- Correct answer
+*   b- 15
+*   c- 16
+*   d- 17
+*   e- 18
+*   f- 19
+*/
