adb6d100205ca83c921f3268acc90f14c6930a0c
Yoiner Bello Fajardo <ybello@imasa.com.ar>
2023-05-08 13:36:29 -0300

    add exercise 22, 23, 24

--- exercise24.java
+++ exercise24.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class exercise24 {
+public class exercise24 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        int c = 8;
+        int d = 4;
+        int result = c++ - ++a + b-- * d;
+        System.out.println(result);
+    }
 }
