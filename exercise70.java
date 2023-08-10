828b3cfc15e71fc8acc0e0aeb2671d53771a290a
Yoiner Bello Fajardo <ybello@imasa.com.ar>
2023-08-10 14:56:55 -0300

    generados los archivos, pendiente de hacer los ejercicios

--- exercise70.java
+++ exercise70.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class exercise70 {
+public class exercise70 {
+    /*
+    *       Diseñar e implementar la clase alimento. Un alimento tiene un nombre y una cantidad de calorias asociadas
+    *       cada 100 gramos.
+     */
+    public static void main(String[] args) {
+
+    }
 }
--- exercise71.java
+++ exercise71.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class exercise71 {
+public class exercise71 {
+    /*
+    *       Escribir en una clase principal la utilizacion de los objetos creados en los dos puntos anteriores.
+     */
+    public static void main(String[] args) {
+
+    }
 }
