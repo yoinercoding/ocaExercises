8763bebacb67e4a7f80f3b4dd20d47bc29705b1d
Yoiner Bello Fajardo <ybello@imasa.com.ar>
2023-09-04 12:13:33 -0300

    challenge exercise

--- exercise70.java
+++
@@ -1,9 +0,0 @@
-public class exercise70 {
-    /*
-    *       Diseñar e implementar la clase alimento. Un alimento tiene un nombre y una cantidad de calorias asociadas
-    *       cada 100 gramos.
-     */
-    public static void main(String[] args) {
-
-    }
-}
