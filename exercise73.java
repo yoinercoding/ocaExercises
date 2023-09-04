f96d507ab4b00fdb6aa143a47dfada4097112b8a
Yoiner Bello Fajardo <ybello@imasa.com.ar>
2023-09-04 12:37:50 -0300

    two exercises

---
+++ exercise73.java
@@ -0,0 +1,14 @@
+public class exercise73 {
+    /*
+     *   Question based on JAVA Programming Language (Book)
+     *             If else clause in Java
+     */
+    public static void main(String[] args) {
+        boolean learning = true;
+        if(learning) {
+            System.out.println("Java Programmer");
+        } else {
+            System.out.println("What are you doing here?");
+        }
+    }
+}
