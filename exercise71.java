af32670614a00bce4546357ff79262f642ddc460
Yoiner Bello Fajardo <yoinercoding@gmail.com>
2023-09-07 11:04:00 -0300

    new exercises 70, 71

--- exercise71.java
+++ exercise71.java
@@ -1,7 +1,4 @@
 public class exercise71 {
-    /*
-    *       Escribir en una clase principal la utilizacion de los objetos creados en los dos puntos anteriores.
-     */
     public static void main(String[] args) {
 
     }
