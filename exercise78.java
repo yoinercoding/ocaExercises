3d83816c25be33b8050fb320d2e5d8099b7e09f7
Yoiner Bello Fajardo <yoinercoding@gmail.com>
2023-09-12 10:32:39 -0300

    new exercise78, streams

---
+++ exercise78.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class exercise78 {
+    /*
+    *       Streams
+    *       Los Java Streams, no son estructuras de datos, sino envolturas alrededor de las fuentes de datos 
+    *       y nos permiten procesar los datos de manera más eficiente.
+     */
+    public static void main(String[] args) {
+
+        Stream<String> streamDeStrings = Stream.of("Antonio", "Maria", "Juan", "Pedro")
+                .filter(s -> s.contains("a"))
+                .map(s -> s.toUpperCase());
+
+        List<String> lista = streamDeStrings.collect(Collectors.toList());
+        System.out.println("Stream con filter y map = " + lista);
+
+        Long streamDeLong = Stream.of("Un long", "Dos long", "Tres long")
+                .count();
+
+        boolean streamDeBoolean = Stream.of("Un boolean", "Dos boolean", "Tres boolean")
+                .anyMatch(s -> s.contains("U"));
+
+        System.out.println("Strean de boolean = " + streamDeBoolean);
+        System.out.println("Stream de long = " + streamDeLong);
+
+        //////////////////////////////////////////////////////////////////////////////////////
+
+        List<String> lista2 = Arrays.asList("Antonio", "Maria", "Juan", "Pedro");
+        System.out.println("Stream con ArrayList = " + lista2);
+
+    }
+}
