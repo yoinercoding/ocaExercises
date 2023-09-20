79b47b2f4fd539c2cb98c2d5f1f26508775e54dc
Yoiner Bello Fajardo <yoinercoding@gmail.com>
2023-09-20 15:56:11 -0300

    new exercise, floyd's triangle

---
+++ exercise81.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class exercise81 {
+    /*
+     *       Floyd's Triangle
+     */
+    public static void main(String[] args) {
+        int n, num = 1, c, d;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Ingresar el numero de filas deseado =");
+        n = in.nextInt();
+        System.out.println("Floyd's triangle =");
+
+        for (c = 1 ; c <= n; c++) {
+            for (d = 1 ; d <= c; d++) {
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}
