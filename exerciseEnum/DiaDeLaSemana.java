8cff7ae37974fafd91960721b8e43f0f57d9533f
Yoiner Bello Fajardo <yoinercoding@gmail.com>
2023-09-21 16:00:10 -0300

    new exercise, enum

--- exerciseEnum/DiaDeLaSemana.java
+++ exerciseEnum/DiaDeLaSemana.java
@@ -1,3 +1,5 @@
+package exerciseEnum;
+
 public enum DiaDeLaSemana {
     LUNES, MARTES, MIERCOLES, JUEVES, VIERNES, SABADO, DOMINGO
 }
--- exerciseEnum/exampleEnum.java
+++ exerciseEnum/exampleEnum.java
@@ -1,4 +1,6 @@
-public class exercise82 {
+package exerciseEnum;
+
+public class exampleEnum {
     /*
     *       Un enumerado (o Enum) es una clase "especial" (tanto en Java como en otros lenguajes) que limitan la
     *       creación de objetos  a los especificados explícitamente en la implementación de la clase.
@@ -6,6 +8,35 @@ public class exercise82 {
     *       este debe de ser privado para que no se puedan crear nuevos objetos.
      */
     public static void main(String[] args) {
+        DiaDeLaSemana hoy = DiaDeLaSemana.DOMINGO;
 
+                //nos trae todos los valores
+//        for(DiaDeLaSemana value : DiaDeLaSemana.values()) {
+//            System.out.println("Hoy es = " + value);
+//        }
+
+        switch (hoy) {
+            case LUNES:
+                System.out.println("Hoy es lunes");
+                break;
+            case MARTES:
+                System.out.println("Hoy es martes");
+                break;
+            case MIERCOLES:
+                System.out.println("Hoy es miercoles");
+                break;
+            case JUEVES:
+                System.out.println("Hoy es jueves");
+                break;
+            case VIERNES:
+                System.out.println("Hoy es viernes");
+                break;
+            case SABADO:
+                System.out.println("Hoy es sabado");
+                break;
+            case DOMINGO:
+                System.out.println("Hoy es domingo");
+                break;
+        }
     }
 }
