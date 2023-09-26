946cefd844beb296a710d4966d661072350762e5
Yoiner Bello Fajardo <yoinercoding@gmail.com>
2023-09-26 14:27:09 -0300

    new exercise get name thread

---
+++ threadExercise/NombreThread.java
@@ -0,0 +1,25 @@
+package threadExercise;
+
+public class NombreThread extends Thread {
+
+    public NombreThread(String str) {
+        super(str);
+    }
+
+    @Override
+    public void run() {
+        for(int i = 0; i < 5; i++) {
+            System.out.println(i + " " + getName());
+        }
+        System.out.println("Termina thread " + getName());
+    }
+
+    public static void main(String[] args) {
+        new NombreThread("Pepe").start();
+        new NombreThread("Pedrito").start();
+        new NombreThread("Juanita").start();
+        new NombreThread("Sabrina").start();
+
+        System.out.println("Termina thread main");
+    }
+}
