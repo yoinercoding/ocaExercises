0b6685a933b4d8640a32029f21f84e3e6087f8da
Yoiner Bello Fajardo <yoinercoding@gmail.com>
2023-09-27 09:38:14 -0300

    exercise number two, synchronized thread

--- synchronizedThreadExercise/contador/Contador.java
+++ synchronizedThreadExercise/contador/Contador.java
@@ -1,2 +1,19 @@
-package synchronizedThreadExercise.contador;public class Contador {
+package synchronizedThreadExercise.contador;
+
+public class Contador {
+    private int contador = 0;
+
+    /*
+    *   Si distintos hilos estan llamando a este metodo al nombrarlo synchronized se ejecuta primero el hilo1, espera
+    *   a que termine y luego se ejecuta el hilo2.
+    *       Puede ser llamado tanto el metodo como en bloque. Ej: synchronized(this) {contador++;}, dará el mismo
+    *   resultado.
+     */
+    public synchronized void sumar() {
+        contador++;
+    }
+
+    public int getContador() {
+        return contador;
+    }
 }
--- synchronizedThreadExercise/contador/Main.java
+++ synchronizedThreadExercise/contador/Main.java
@@ -1,2 +1,31 @@
-package synchronizedThreadExercise.Contador;public class Main {
+package synchronizedThreadExercise.contador;
+
+public class Main {
+    public static void main(String[] args) {
+        Contador contador = new Contador();
+
+        Thread hilo1 = new Thread(() -> {
+            for(int i = 0; i < 10000; i++) {
+                contador.sumar();
+            }
+        });
+
+        Thread hilo2 = new Thread(() -> {
+            for(int i = 0; i < 10000; i++) {
+                contador.sumar();
+            }
+        });
+
+        hilo1.start();
+        hilo2.start();
+
+        try {
+            hilo1.join();
+            hilo1.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e.toString());
+        }
+
+        System.out.println("El contador vale " + contador.getContador());
+    }
 }
