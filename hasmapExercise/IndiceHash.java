c84f3d9e5f5d71ebf46dec6595dab7816d2fd9f3
Yoiner Bello Fajardo <yoinercoding@gmail.com>
2023-10-02 11:52:02 -0300

    update, indiceHash

--- hasmapExercise/IndiceHash.java
+++ hasmapExercise/IndiceHash.java
@@ -1,2 +1,27 @@
-package hasmapExercise;public class IndiceHash {
+package hasmapExercise;
+
+import java.util.Objects;
+
+    /*
+    *       Tambien se pueden agregar valores con otra clase con equals y hashCode
+    */
+
+public class IndiceHash {
+    private Integer indice;
+
+    public IndiceHash(int i) {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        IndiceHash that = (IndiceHash) o;
+        return Objects.equals(indice, that.indice);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(indice);
+    }
 }
